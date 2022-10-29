49840c7544abd6cea9670dbc520503f0277f0a7d
Mariama DIALLO <diallo.mbobo108@gmail.com>
2022-10-29 21:26:04 +0200

    création de la classe Humain

--- src/Personnages/Humain.java
+++ src/Personnages/Humain.java
@@ -35,19 +35,19 @@ public class Humain{
 	
 	
 	public void gagnerArgent(int gain){
-		gain+=gain;
+		argent+=gain;
 	}
 	
 
 	public void perdreArgent(int perte){
-		perte-=1;
+		argent-=1;
 	
 }
 	public void direBonjour(){
-		System.out.println("Bonjour! je m'appelle " + nom +"  et j'aime boire du" + boissonFav );
+		System.out.println(prendreParole()+"Bonjour! je m'appelle " + nom +"  et j'aime boire du" + boissonFav );
 	}
 	public void Boire(){
-		System.out.println("Mmmm, un bon verre de "+ boissonFav + "!GLOUPS!");	
+		System.out.println(prendreParole()+"Mmmm, un bon verre de "+ boissonFav + "!GLOUPS!");	
 	}
 
 	public void acheter(String bien, int prix){
@@ -58,8 +58,8 @@ public class Humain{
 			argent=argent-prix;
 		}
 		else{
-			System.out.println(prendreParole() + "Je n'ai plus que" + argent +
-					" sous en poche. Je ne peux même pas m'offrir un " + bien + " à " +" sous " );
+			System.out.println(prendreParole() + "Je n'ai plus que " + argent +
+					" sous en poche. Je ne peux même pas m'offrir un " + bien + " à " + prix +" sous " );
 		}
 	}
 
--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -6,6 +6,7 @@ public class HistoireTP4{
 	public static void main(String[] args){
 		
 		Humain prof = new Humain(" prof", " kombucha", 54);
+		prof.direBonjour();
 		prof.acheter(" boison", 12);
 		prof.Boire();
 		prof.acheter("jeu", 2);
